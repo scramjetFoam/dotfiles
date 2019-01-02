8253a0e509a350cfcd2c9ce55231425f9b3a0c41
Vítor Galvão <info@vitorgalvao.com>
2019-01-02 20:54:00 +0000

    Switch to version managed languages

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -4,19 +4,37 @@ install_brew() {
 }
 
 install_python() {
-  brew install python
-  # install some eggs
-  pip3 install instapy-cli neovim
+  brew install pyenv
+
+  pyenv install "$(pyenv install --list | sed 's/^  //' | grep '^\d' | grep --invert-match 'dev\|a\|b' | tail -1)" # install latest stable python
+  pyenv global $(pyenv versions | tail -1) # switch to latest installed python
+  eval "$(pyenv init -)" # activate pyenv
+
+  # Install some eggs
+  pip install eyed3 instapy-cli neovim subliminal
 }
 
 install_ruby() {
-  brew install ruby
-  # install some gems
+  brew install chruby ruby-install
+
+  ruby-install --src-dir "$(mktemp -d)" ruby # install latest stable ruby
+  source /usr/local/opt/chruby/share/chruby/chruby.sh # activate chruby
+  chruby ruby # switch to latest installed ruby
+
+  # Install some gems
   gem install --no-document neovim nokogiri pry redcarpet ronn rubocop video_transcoding watir
 }
 
 install_node() {
-  brew install node
+  brew install nvm
+
+  # activate nvm
+  mkdir "${HOME}/.nvm"
+  export NVM_DIR="${HOME}/.nvm"
+  [ -s '/usr/local/opt/nvm/nvm.sh' ] && source '/usr/local/opt/nvm/nvm.sh'
+
+  nvm install node # install latest node
+
   # install some packages
   npm install --global eslint eslint-plugin-immutable eslint-plugin-shopify jsonlint neovim nightmare prettier
 }
